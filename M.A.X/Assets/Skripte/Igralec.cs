98570f449b901231d5dff23987a144e9cb9b9a57
LBTMedo <banko.jan@gmail.com>
2016-10-22 16:42:15 +0200

    Prvi commit.
    Osnovni projekt in skripte.

---
+++ M.A.X/Assets/Skripte/Igralec.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(IgralecKontroler))]
+public class Igralec : Subjekt {
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public override void Smrt()
+    {
+        base.Smrt();
+    }
+
+}
---
+++ M.A.X/Assets/Skripte/Subjekt.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Subjekt : MonoBehaviour {
+
+    public float zacetnaZivljenja;
+    public float trenutnaZivljenja { get; protected set; }
+    protected bool mrtev = false;
+
+    public event System.Action ObSmrti;
+
+	void Start () {
+        trenutnaZivljenja = zacetnaZivljenja;
+	}
+
+    public void PrejmiSkodo(float skoda)
+    {
+        trenutnaZivljenja -= skoda;
+        if(trenutnaZivljenja <= 0 && !mrtev)
+        {
+            Smrt();
+        }
+    }
+	
+	public virtual void Smrt()
+    {
+        mrtev = true;
+        if(ObSmrti != null)
+        {
+            ObSmrti();
+        }
+        Destroy(gameObject);
+    }
+}
