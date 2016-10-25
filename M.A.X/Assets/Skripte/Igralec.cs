f90bff0e874db61bce8d0fd058fcf5357d8d964b
RokPot <rokypot@gmail.com>
2016-10-25 18:51:01 +0200

    Nič ne dela

--- M.A.X/Assets/Skripte/Igralec.cs
+++ M.A.X/Assets/Skripte/Igralec.cs
@@ -4,10 +4,33 @@ using System.Collections;
 [RequireComponent(typeof(IgralecKontroler))]
 public class Igralec : Subjekt {
 
+    public int[] poljeIntov;
+    public string[] poljeStringov;
+
 	void Start ()
     {
-	
-	}
+        poljeIntov = new int[10];
+        poljeStringov = new string[5];
+
+        for(int i = 0; i < poljeIntov.Length; i++)
+        {
+            poljeIntov[i] = Mathf.RoundToInt(Random.Range(1, 100));
+        }
+
+        for (int i = 0; i < poljeIntov.Length; i++)
+        {
+            Debug.Log(poljeIntov[i]);
+        }
+
+        Debug.Log("-----------------");
+
+        Utility.Uredi(poljeIntov, false);
+
+        for (int i = 0; i < poljeIntov.Length; i++)
+        {
+            Debug.Log(poljeIntov[i]);
+        }
+    }
 	
 	void Update ()
     {
---
+++ M.A.X/Assets/Skripte/Utility.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Utility : MonoBehaviour {
+
+    static void Swap<T>(ref T prvi, ref T drugi)
+    {
+        T zacasni;
+        zacasni = prvi;
+        prvi = drugi;
+        drugi = zacasni;
+    }
+
+    public static void Uredi<T>(T[] polje, bool narascajoce) where T : IComparable
+    {
+        for(int i = 0; i < polje.Length; i++)
+        {
+            for(int j = 0; j < polje.Length - 1; j++)
+            {
+                if (narascajoce)
+                {
+                    if(polje[j].CompareTo(polje[j+1]) > 0)
+                    {
+                        Swap<T>(ref polje[j], ref polje[j + 1]);
+                    }
+                }
+                else if (!narascajoce)
+                {
+                    if (polje[j].CompareTo(polje[j + 1]) < 0)
+                    {
+                        Swap<T>(ref polje[j], ref polje[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
