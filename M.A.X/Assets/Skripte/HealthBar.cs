b573dfe66f76b6f16af9ab8d6f89c2e6b92f4cdc
lukiinc <lukapecnik96@gmail.com>
2016-10-27 18:19:54 +0200

    Dodan Health Bar in osnovna skripta zanj

---
+++ M.A.X/Assets/Skripte/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image fill;
+	
+	// Update is called once per frame
+	void Update () {
+        HandleBar();
+	}
+
+    private void HandleBar()
+    {
+        fill.fillAmount = fillAmount;  //tu bom uporabo funkcijo ConvertHealth
+    }
+
+    private float ConvertHealth(float currHealth, float healthMin, float healthMax, float scaleMin, float scaleMax)
+    {
+        return (currHealth - healthMin) * (scaleMax - scaleMin) / (healthMax - healthMin) + scaleMin;
+    }
+}
