931d75a603e688ce069249eec951ae2ce1ac899e
RokPot <rokypot@gmail.com>
2016-10-31 18:28:05 +0100

    SkripteZainterface

--- M.A.X/Assets/Skripte/Komentarji/Rok.cs
+++ M.A.X/Assets/Skripte/Komentarji/Rok.cs
@@ -1,1 +1,11 @@
-﻿
+﻿/*
+https://unity3d.com/learn/tutorials/topics/2d-game-creation/2d-physics-overview?playlist=17093
+https://unity3d.com/learn/tutorials/topics/2d-game-creation/rigidbody-2d?playlist=17093
+https://unity3d.com/learn/tutorials/topics/2d-game-creation/area-effector-2d?playlist=17093
+https://unity3d.com/learn/tutorials/topics/2d-game-creation/bouncing-sliding-2d?playlist=17093
+https://unity3d.com/learn/tutorials/topics/2d-game-creation/collider-2d?playlist=17093
+https://www.youtube.com/watch?v=m0fjrQkaES4 Collider
+
+
+
+*/
---
+++ M.A.X/Assets/Skripte/Labirint3Gumbi.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+    public class Labirint3Gumbi : MonoBehaviour
+    {
+
+        public GameObject Trigger3;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        void OnTriggerEnter2D(Collider2D coll)
+        {
+
+            if (coll.name == Trigger3.name)
+            {
+                Debug.Log("Trigger3", gameObject);
+            }
+            else
+            {
+                Debug.Log("Trigger1", gameObject);
+
+            }
+
+
+        }
+    }
+
