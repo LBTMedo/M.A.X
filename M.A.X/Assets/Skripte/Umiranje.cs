2b660974658c19c0c9fea1bc18d095edab09b040
RokPot <rokypot@gmail.com>
2016-11-02 10:29:30 +0100

    update

---
+++ M.A.X/Assets/Skripte/Umiranje.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Umiranje : MonoBehaviour {
+
+    private Igralec player;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Igralec>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        player.PrejmiSkodo(1111);
+    }
+}
