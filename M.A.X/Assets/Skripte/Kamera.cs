2ed40a77fc2899a48cdea618eb414b1ec286e6a9
RokPot <rokypot@gmail.com>
2016-11-07 22:28:26 +0100

    Izdelava odzadja Alpha

---
+++ M.A.X/Assets/Skripte/Kamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kamera : MonoBehaviour {
+    public Transform player;
+    public float damping = 1;
+    public float GledajeNaprejFaktor = 3;
+    public float VracanjeKamereHitrost = 0.5f;
+    public float PragKamere = 0.1f;
+
+    float offsetZ;
+    Vector3 ZadnjaPozicija;
+    Vector3 Hitrost;
+    Vector3 NaslednjaPozicija;
+    // Use this for initialization
+    void Start () {
+        ZadnjaPozicija = player.position;
+        offsetZ = (transform.position - player.position).z;
+        transform.parent = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float xMoveDelta = (player.position - ZadnjaPozicija).x;
+        bool GledanjeNaprej = Mathf.Abs(xMoveDelta) > PragKamere;
+        if (GledanjeNaprej)
+        {
+            NaslednjaPozicija = GledajeNaprejFaktor * Vector3.right * Mathf.Sign(xMoveDelta);       
+        }
+        else
+        {
+            NaslednjaPozicija = Vector3.MoveTowards(NaslednjaPozicija, Vector3.zero, Time.deltaTime * VracanjeKamereHitrost);
+        }
+        Vector3 nextPozicija = player.position + NaslednjaPozicija + Vector3.forward * offsetZ;
+        Vector3 novaPozicija = Vector3.SmoothDamp(transform.position, nextPozicija, ref Hitrost, damping);
+        transform.position = novaPozicija;
+        ZadnjaPozicija = player.position;
+    }
+}
--- M.A.X/Assets/Skripte/Komentarji/Rok.cs
+++ M.A.X/Assets/Skripte/Komentarji/Rok.cs
@@ -1,1 +1,8 @@
-﻿
+﻿/* 2. Teden
+
+https://www.youtube.com/watch?v=zwNGjS_1Lxk&index=2&list=PLPV2KyIb3jR42oVBU6K2DIL6Y22Ry9J1c Osnova za background, izdelava sorting layerja
+https://unity3d.com/learn/tutorials/topics/2d-game-creation/sprite-editor rezanje slike
+
+
+
+*/
