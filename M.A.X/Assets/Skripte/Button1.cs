40a4e2cfec3f4f9f04ffbfc34d4796c3f3fcb79b
Policaj <timotej.cesar@gmail.com>
2016-11-16 15:36:49 +0100

    lvl nadgradnja

---
+++ M.A.X/Assets/Skripte/Button1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button1 : MonoBehaviour {
+
+    public bool pressed;
+    public float distance = 15f;
+
+	// Use this for initialization
+	void Start () {
+        pressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (pressed)
+        {
+            transform.Translate(-Vector2.up * Time.deltaTime * distance);
+        }
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        pressed = true;
+    }
+}
--- M.A.X/Assets/Skripte/MovingPlatforms.cs
+++ M.A.X/Assets/Skripte/MovingPlatforms.cs
@@ -6,7 +6,8 @@ public class MovingPlatforms : MonoBehaviour {
     public Transform end;
     private Vector3 endpos;
     private Vector3 start;
-    public float speed = 1f;
+    private float speed = 1f;
+    public float moveTime = 1f;
     private float pingpong;
 	// Use this for initialization
 	void Start () {
@@ -16,8 +17,8 @@ public class MovingPlatforms : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        pingpong = Mathf.PingPong(Time.time * speed, 1f);
-        //transform.position = new Vector3(Mathf.PingPong(Time.time, (transform.position.x + distance) - transform.position.x) + transform.position.x, transform.position.y, transform.position.z);
+        pingpong = Mathf.PingPong(Time.time * speed / moveTime, 1f);
+        
         transform.position = Vector3.Lerp(start, endpos, Mathf.SmoothStep(0f, 1f, pingpong));
         transform.position = Vector3.Lerp(endpos, start, Mathf.SmoothStep(0f, 1f, pingpong));
 
