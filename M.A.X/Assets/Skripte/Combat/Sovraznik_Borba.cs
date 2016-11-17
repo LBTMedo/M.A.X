296fcd12d64a925400e20b66402758094ee1aa5d
LBTMedo <banko.jan@gmail.com>
2016-11-17 11:13:41 +0100

    Popravki - streljanje sovražnik

--- M.A.X/Assets/Skripte/Combat/Sovraznik_Borba.cs
+++ M.A.X/Assets/Skripte/Combat/Sovraznik_Borba.cs
@@ -39,19 +39,25 @@ public class Sovraznik_Borba : MonoBehaviour {
             }
 
             yield return new WaitForSeconds(0.5f);
+
         }
     }*/
+
+    void Update()
+    {
+        desno = !sovraznik.left;
+    }
 
     public void Streljanje()
     {
         Rigidbody2D instancaMetka = Instantiate(metek, tockaZaStreljanje.position, tockaZaStreljanje.rotation) as Rigidbody2D;
-        if (!sovraznik.left)
+        //if (desno)
+        //{
+            instancaMetka.velocity = hitrostMetka * transform.right;
+       //}
+        /*else
         {
-            instancaMetka.velocity = hitrostMetka * instancaMetka.transform.right;
-        }
-        else if (sovraznik.left)
-        {
-            instancaMetka.velocity = hitrostMetka * (-instancaMetka.transform.right);
-        }
+            instancaMetka.velocity = hitrostMetka * (transform.right);
+        }*/
     }
 }
