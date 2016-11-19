5342283728b6cb7943aad379c9007c3ae13363d2
lukiinc <lukapecnik96@gmail.com>
2016-11-19 10:52:55 +0100

    BossFight camera shake končan

--- M.A.X/Assets/Skripte/Boss scene/ButtonBoss.cs
+++ M.A.X/Assets/Skripte/Boss scene/ButtonBoss.cs
@@ -12,17 +12,22 @@ public class ButtonBoss : MonoBehaviour
     public Vector3 defaultButtonPos;
     Vector3 newButtonPos;
 
+    [SerializeField]
+    GameObject cameraToShake;
+
     void Start()
     {
         objekti = FindObjectOfType<RandomFlyingObjects>();
         defaultButtonPos = gumb.transform.position;
         newButtonPos = new Vector3(defaultButtonPos.x, defaultButtonPos.y - 0.20f, defaultButtonPos.z);
+        
     }
 
     void OnCollisionEnter2D()
     {
         if (objekti.Reloaded())
         {
+            cameraToShake.GetComponent<CameraShake>().ShakeCamera(10f, 0.05f);
             objekti.GenerateAndDropObjects();
             gumb.transform.position = newButtonPos;
         }
