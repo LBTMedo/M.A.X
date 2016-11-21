95516eaf6f476e00860ad07f4826f0284f1ac0ab
RokPot <rokypot@gmail.com>
2016-11-21 19:26:30 +0100

    Zvok1

--- M.A.X/Assets/Skripte/CoinPickup.cs
+++ M.A.X/Assets/Skripte/CoinPickup.cs
@@ -4,11 +4,18 @@ using System.Collections;
 public class CoinPickup : MonoBehaviour {
 
     public int value = 1;
-
+    public AudioClip zvokCoin;
+    private AudioSource source;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
+
         if(other.tag == "Player")
         {
+            source.PlayOneShot(zvokCoin, 1F);
             GameManager.DodajDenar(value);
             Destroy(gameObject);
         }
---
+++ M.A.X/Assets/Skripte/SliderZvokMaster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Slider mainSlider;
+
+    //Invoked when a submit button is clicked.
+    public void SubmitSliderSetting()
+    {
+        //Displays the value of the slider in the console.
+        Debug.Log(mainSlider.value);
+    }
+}
