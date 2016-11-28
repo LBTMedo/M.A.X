ceedcbdee1ef9de516cf75bb75db516ae6c99f76
LBTMedo <banko.jan@gmail.com>
2016-11-28 17:48:54 +0100

    Boss01
    Minion skripta, manager

---
+++ M.A.X/Assets/Skripte/Combat/BSManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BSManager : MonoBehaviour {
+
+    private Igralec player;
+
+    public Transform spawnPoint;
+
+    public GameObject minion;
+
+    public float timer = 3f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = FindObjectOfType<Igralec>();
+        SpawnEnemy();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            timer = 3f;
+            //SpawnEnemy();
+        }
+	}
+
+    void SpawnEnemy()
+    {
+        Rigidbody2D enemy = Instantiate(minion, spawnPoint.position, spawnPoint.rotation) as Rigidbody2D;
+    }
+}
