d9aa1ee84ee652f11c919fa3fd04c813049fe05d
lukiinc <lukapecnik96@gmail.com>
2016-12-01 18:42:22 +0100

    Nadaljevanje co-op levela #1

---
+++ M.A.X/Assets/LeverDoor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverDoor : MonoBehaviour {
+
+    private bool odprto = false;
+
+    Vector3 closedPos;
+    Vector3 openPos;
+    public float premikHorizontalno;
+    public float premikVertikalno;
+
+    void Start()
+    {
+        closedPos = transform.position;
+        openPos = new Vector3(closedPos.x - premikHorizontalno, closedPos.y - premikVertikalno, closedPos.z);
+    }
+
+    public void SetOdprto(bool value)
+    {
+        odprto = value;
+    }
+    public bool GetOdprto()
+    {
+        return odprto;
+    }
+
+    void OpenDoor()
+    {
+        transform.position = openPos;
+    }
+
+    void CloseDoor()
+    {
+        transform.position = closedPos;
+    }
+
+    void Update()
+    {
+        if(odprto == true)
+        {
+            OpenDoor();
+        }
+        else if(odprto == false)
+        {
+            CloseDoor();
+        }
+    }
+}
---
+++ M.A.X/Assets/Skripte/Lever.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Lever : MonoBehaviour {
+
+    [SerializeField]
+    private Text reminder;
+
+    [SerializeField]
+    private LeverDoor slideDoor;
+
+    [SerializeField]
+    Sprite activated;
+    [SerializeField]
+    Sprite deactivated;
+
+    SpriteRenderer slikaLever;
+
+    bool entered = false;
+
+    void Start()
+    {
+        slikaLever = GetComponent<SpriteRenderer>();
+    }
+
+	void OnTriggerEnter2D()
+    {
+        //reminder.text = "press 'F'";
+        entered = true;
+    }
+    void OnTriggerExit2D()
+    {
+        //reminder.text = "";
+        entered = false;
+    }
+
+    void Update()
+    {
+        if(entered == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F) && slideDoor.GetOdprto() == false)
+            {
+                slideDoor.SetOdprto(true);
+                slikaLever.sprite = activated;
+            }
+            else if(Input.GetKeyDown(KeyCode.F) && slideDoor.GetOdprto() == true)
+            {
+                slideDoor.SetOdprto(false);
+                slikaLever.sprite = deactivated;
+            }
+        }
+    }
+
+}
