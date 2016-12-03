3d5b4b564683bd59487a88783988a584621f0de8
lukiinc <lukapecnik96@gmail.com>
2016-12-03 13:17:13 +0100

    Nadaljevanje co-op levela KONEC

---
+++ M.A.X/Assets/Skripte/CoopSkripte/CoopGameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoopGameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
