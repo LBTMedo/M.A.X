84bd6f5b2b6a410c8f616c71f8d36e80af3a7398
Policaj <timotej.cesar@gmail.com>
2016-12-04 14:35:04 +0100

    opt

---
+++ M.A.X/Assets/Skripte/Artefacts.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Artefacts : MonoBehaviour {
+
+    public bool[] artefacts;
+    public bool full = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
