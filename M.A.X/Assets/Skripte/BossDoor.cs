7be582187ec78a18399543029db8185bd80ff081
Policaj <timotej.cesar@gmail.com>
2016-12-05 17:02:55 +0100

    optimizacija 2

---
+++ M.A.X/Assets/Skripte/BossDoor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossDoor : MonoBehaviour {
+
+    public bool open;
+    private Animator anim;
+    
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (open)
+        {
+            Debug.Log("Opening boss door");
+            anim.SetBool("open", true);
+        }
+	}
+}
