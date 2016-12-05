ea39144447eb027db630697d3a7c058a00de78ca
Policaj <timotej.cesar@gmail.com>
2016-12-05 16:42:37 +0100

    opt2

--- M.A.X/Assets/Skripte/Chest.cs
+++ M.A.X/Assets/Skripte/Chest.cs
@@ -5,7 +5,9 @@ public class Chest : MonoBehaviour {
 
     public Sprite OpenSprite;
     private SpriteRenderer renderer;
+    public GameObject drop;
     public GameObject trigger;
+    private int count = 0;
     private bool open = false;
 	// Use this for initialization
 	void Start () {
@@ -15,9 +17,11 @@ public class Chest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         open = trigger.GetComponent<DoorTrigger>().entered;
-        if (open)
+        if (open && count == 0)
         {
             renderer.sprite = OpenSprite;
+            Instantiate(drop, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.rotation);
+            count++;
         }
 	}
 }
