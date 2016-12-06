4de7f31923bf8f4c066d451aa02d8bdeaa3eaa50
RokPot <rokypot@gmail.com>
2016-12-06 23:50:48 +0100

    Zacetek Minigame-a 3

---
+++ M.A.X/Assets/Skripte/Minigame/PremikajKamero.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PremikajKamero : MonoBehaviour {
+
+    private float zoomSpeed = 2.0f;
+
+    public float KameraXObc = 100.0f;
+    public float KameraYObc = 100.0f;
+
+
+    private float speed = 3.0f;
+    private float scroll = 5f;
+    void Update()
+    {
+        scroll += -Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.orthographicSize = scroll * zoomSpeed;
+
+        if (Input.GetMouseButton(1))
+        {
+
+            float rotationX = Input.GetAxis("Mouse X") * KameraXObc * Time.deltaTime;
+            float rotationY = Input.GetAxis("Mouse Y") * KameraYObc * Time.deltaTime;
+            transform.Translate(new Vector3(-rotationX, -rotationY, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
