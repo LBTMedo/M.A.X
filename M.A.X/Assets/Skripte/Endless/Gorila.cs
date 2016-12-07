4b381a28b2ee767c666efd2981d83fa085b5d534
LBTMedo <banko.jan@gmail.com>
2016-12-07 13:04:55 +0100

    Endless2.0 (1)
    Gorila, sledenje
    Kamera

---
+++ M.A.X/Assets/Skripte/Endless/Gorila.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Gorila : MonoBehaviour {
+
+    EndlessPC igralec;
+
+    Queue<Vector3> pozicije;
+
+    float timer;
+    bool zagnano;
+
+    public bool zacetek;
+
+    private void Start()
+    {
+        igralec = FindObjectOfType<EndlessPC>();
+
+        timer = 2f;
+        zagnano = false;
+
+        pozicije = new Queue<Vector3>();
+
+        zacetek = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (zacetek)
+        {
+            DobiPozicijoIgralca();
+            if (zagnano)
+            {
+                Sledi();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (!zagnano && zacetek)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0f)
+            {
+                zagnano = true;
+            }
+        }
+    }
+
+    void DobiPozicijoIgralca()
+    {
+        pozicije.Enqueue(igralec.transform.position);
+    }
+
+    void Sledi()
+    {
+        transform.position = pozicije.Dequeue();
+    }
+}
