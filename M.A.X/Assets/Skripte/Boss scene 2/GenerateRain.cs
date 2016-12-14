fc1e2d2948673d9b122687f04ee6d1745d824dfb
lukiinc <lukapecnik96@gmail.com>
2016-12-14 23:22:42 +0100

    Začetek Boss battle 2

---
+++ M.A.X/Assets/Skripte/Boss scene 2/GenerateRain.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateRain : MonoBehaviour {
+
+    public GameObject prefab;
+    public int numOfRepetitions;
+    public int numOfDrops;
+    public float heightOfGeneration;
+    public float minX;
+    public float maxX;
+
+    bool reloading = false;
+
+    void Update()
+    {
+        if (!reloading)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    public void GenerateRainDrops()
+    {
+        //int changeNumOfDrops = numOfRepetitions / 2;
+        //for (int j = 0; j < numOfRepetitions; j++)
+        //{
+            for (int i = 0; i < numOfDrops; i++)
+            {
+                float posX = Random.Range(minX, maxX);
+                Vector3 pozicija = new Vector3(posX, heightOfGeneration, 0);
+                Instantiate(prefab, pozicija, Quaternion.identity);
+            }
+           /* if(numOfRepetitions < changeNumOfDrops)
+            {
+                numOfDrops *= (int)1.5;
+            }
+            else
+            {
+                numOfDrops /= (int)1.5;
+            }*/
+       // }
+    }
+
+    IEnumerator Reload()
+    {
+        int restoreNumOfDrops = numOfDrops;
+        int changeNumOfDrops = numOfRepetitions / 2;
+        for (int i = 0; i < numOfRepetitions; i++)
+        {
+            GenerateRainDrops();
+            if (numOfRepetitions < changeNumOfDrops)
+            {
+                numOfDrops *= (int)1.2;
+            }
+            else
+            {
+                numOfDrops = numOfDrops/(int)1.2;
+            }
+            yield return new WaitForSeconds(1);
+        }
+        numOfDrops = restoreNumOfDrops;
+        reloading = true;
+        yield return new WaitForSeconds(5);
+        reloading = false;
+    }
+}
---
+++ M.A.X/Assets/Skripte/Boss scene 2/RainDrop.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainDrop : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag != "Brick")
+        Destroy(this.gameObject);
+    }
+}
