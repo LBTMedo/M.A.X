ea05b392336561cc8aa3dcac48d16f9585494485
lukiinc <lukapecnik96@gmail.com>
2016-12-15 14:27:53 +0100

    2. boss scena #3

---
+++ M.A.X/Assets/Skripte/Boss scene 2/ThrowPleagueBall.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowPleagueBall : MonoBehaviour {
+
+    public GameObject prefab;
+    float randomTimeSpawn = 0;
+    bool cooldown;
+    public float minTimeSpawn;
+    public float maxTimeSpawn;
+
+    public void ThrowBall()
+    {
+        randomTimeSpawn = Random.Range(minTimeSpawn, maxTimeSpawn);
+
+
+    }
+}
