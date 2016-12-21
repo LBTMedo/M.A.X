41ac39f1b83c5cc41e4f065f08550d4dd9d58292
lukiinc <lukapecnik96@gmail.com>
2016-12-21 16:02:03 +0100

    Medieval Scene #2

---
+++ M.A.X/Assets/MoveWithTranslate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithTranslate : MonoBehaviour {
+
+    public float speed;
+    public bool left = true;
+
+	void FixedUpdate()
+    {
+        if (left)
+            transform.Translate(Time.deltaTime*speed, 0, 0);
+        else
+            transform.Translate(-(Time.deltaTime * speed), 0, 0);
+    }
+}
