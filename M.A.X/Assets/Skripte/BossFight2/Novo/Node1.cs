e77dcfb9811c6b8936e2300b144980a11a3c21ba
LBTMedo <banko.jan@gmail.com>
2016-12-22 20:47:27 +0100

    A*.2

---
+++ M.A.X/Assets/Skripte/BossFight2/Novo/Node1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node1 {
+
+    public bool walkable;
+    public Vector2 worldPosition;
+    public int gridX;
+    public int gridY;
+
+    public int gCost;
+    public int hCost;
+    public Node1 parent;
+
+    public Node1(bool _walkable, Vector2 _worldPos, int _gridX, int _gridY)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+}
